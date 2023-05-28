40261f737ed8936a729e1e4691ac49593b6e6218
saus1 <suburbanosanchez@frba.utn.edu.ar>
2023-05-27 23:45:12 -0300

    prueba en ventas

---
+++ scrips/products.js
@@ -0,0 +1,33 @@
+
+const productos = [
+  {
+    id: 1,
+    nombre: "Aire Acondicionado",
+    precio: 380000,
+    img:
+      "ImagenDeProductos/productoAireAcondicionado.png",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Cafetera",
+    precio: 80000,
+    img:
+      "ImagenDeProductos/productoCafetera.png",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Cocina",
+    precio: 270000,
+    img:
+      "ImagenDeProductos/productoCocina.png",
+  },
+  {
+    id: 4,
+    nombre: "Tv Led",
+    precio: 200,
+    img:
+      "ImagenDeProductos/productoTvLed.png",
+  },
+];
